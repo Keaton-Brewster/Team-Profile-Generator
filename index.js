34cd4908009e3a80fbeffd7e459502d4764a9351
Delta9k8b <keaton.brewster@gmail.com>
2021-02-15 19:55:39 -0600

    setting up inital classes for employees and employee types

---
+++ index.js
@@ -0,0 +1,6 @@
+const Employee = require("./lib/Employee"),
+  Engineer = require("./lib/Engineer"),
+  Intern = require("./lib/Intern"),
+  Manager = require("./lib/Manager"),
+  Chalk = require("chalk"),
+  Inq = require('inquirer');
