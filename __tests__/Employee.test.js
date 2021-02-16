f792027e966a5c22263c811f126252871fbabe02
Delta9k8b <keaton.brewster@gmail.com>
2021-02-16 09:22:17 -0600

    added tests to make sure the class constructors are working

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,48 @@
 const Employee = require("../lib/Employee");
+
+describe("Employee class", () => {
+  it("Should create a new object", () => {
+    let emp = new Employee();
+
+    expect(emp).toBeInstanceOf(Employee);
+  });
+  it("Should accept three arguments: name, id, and email and store those as the properties of the new objetct", () => {
+    const thisName = "Keaton";
+    const thisID = 3;
+    const thisEmail = "Keaton.brewster@gmail.com";
+
+    const emp = new Employee(thisName, thisID, thisEmail);
+
+    expect(emp.name).toEqual(thisName);
+    expect(emp.id).toEqual(thisID);
+    expect(emp.email).toEqual(thisEmail);
+  });
+  describe("getName", () => {
+    it("Should return the name inputted into the new employee constructor", () => {
+      const emp = new Employee("Keaton");
+
+      expect(emp.getName()).toEqual("Keaton");
+    });
+  });
+  describe("getId", () => {
+    it("Should return the id inputted into the employee constructor", () => {
+      const emp = new Employee("NAME", 3);
+
+      expect(emp.getId()).toEqual(3);
+    });
+  });
+  describe("getEmail", () => {
+    it("Should return the email address inputted into the employee constructor", () => {
+      const emp = new Employee("NAME", "ID", "EMAIL");
+
+      expect(emp.getEmail()).toEqual("EMAIL");
+    });
+  });
+  describe("getRole", () => {
+    it("Should return 'Employee' when called", () => {
+      const emp = new Employee();
+
+      expect(emp.getRole()).toEqual("Employee");
+    });
+  });
+});
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,2 +1,64 @@
 const Employee = require("../lib/Employee");
-const Engineer = require('../lib/Engineer')
+const engineer = require("../lib/Engineer");
+const Engineer = engineer.Engineer;
+
+describe("Engineer class", () => {
+  it("should be an extension of the Employee class", () => {
+    const emp = new Engineer();
+
+    expect(emp).toBeInstanceOf(Employee);
+  });
+  it("Should create a new instance of the Engineer class", () => {
+    const emp = new Engineer();
+
+    expect(emp).toBeInstanceOf(Engineer);
+  });
+  it("Should take in four arguments as details for the Engineer", () => {
+    const thisName = "Keaton",
+      thisID = 5,
+      thisEmail = "Email",
+      thisGithub = "Github Address";
+
+    const emp = new Engineer(thisName, thisID, thisEmail, thisGithub);
+
+    expect(emp.name).toEqual(thisName);
+    expect(emp.id).toEqual(thisID);
+    expect(emp.email).toEqual(thisEmail);
+    expect(emp.githubUsername).toEqual(thisGithub);
+  });
+  describe("getName", () => {
+    it("Should return the name inputted into the new employee constructor", () => {
+      const emp = new Engineer("Keaton");
+
+      expect(emp.getName()).toEqual("Keaton");
+    });
+  });
+  describe("getId", () => {
+    it("Should return the id inputted into the employee constructor", () => {
+      const emp = new Engineer("NAME", 3);
+
+      expect(emp.getId()).toEqual(3);
+    });
+  });
+  describe("getEmail", () => {
+    it("Should return the email address inputted into the employee constructor", () => {
+      const emp = new Engineer("NAME", "ID", "EMAIL");
+
+      expect(emp.getEmail()).toEqual("EMAIL");
+    });
+  });
+  describe("getRole", () => {
+    it("Should return 'Engineer' when called", () => {
+      const emp = new Engineer();
+
+      expect(emp.getRole()).toEqual("Engineer");
+    });
+  });
+  describe("getGithub", () => {
+    it("Should return the github username of the Engineer", () => {
+      const emp = new Engineer("NAME", "ID", "EMAIL", "github username");
+
+      expect(emp.githubUsername).toEqual("github username");
+    });
+  });
+});
