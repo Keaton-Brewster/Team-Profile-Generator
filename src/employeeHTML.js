4b8a58de61f4a5197dde41429d2b5485d7c21b24
Delta9k8b <keaton.brewster@gmail.com>
2021-02-17 06:36:13 -0600

    formatting

--- src/employeeHTML.js
+++ src/employeeHTML.js
@@ -1,6 +1,7 @@
 // Here I have all the functions for generating a snippet of html based on which type of new employee is added
 function generateManagerHTML(manager) {
-  return `  <div class="card col-3 p-0 m-1">
+  return `  
+  <div class="card col-3 p-0 m-1">
                 <div class="card-header bg-dark">
                     <h2 class="card-title">${manager.name}</h2>
                     <p>${manager.getRole()}</p>
@@ -9,15 +10,20 @@ function generateManagerHTML(manager) {
 
                     <ul class="list-group">
                         <li class="list-group-item">ID: ${manager.id}</li>
-                        <li class="list-group-item">Email: <a href="${manager.getEmail()}" target="blank">${manager.email}</a></li>
-                        <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+                        <li class="list-group-item">Email: <a href="${manager.getEmail()}" target="blank">${
+    manager.email
+  }</a></li>
+                        <li class="list-group-item">Office Number: ${
+                          manager.officeNumber
+                        }</li>
                     </ul>
                 </div>
             </div>`;
 }
 
 function generateEngineerHTML(engineer) {
-  return `  <div class="card col-3 p-0 m-1">
+  return `  
+  <div class="card col-3 p-0 m-1">
                 <div class="card-header bg-dark">
                     <h2 class="card-title">${engineer.name}</h2>
                     <p>${engineer.getRole()}</p>
@@ -26,15 +32,20 @@ function generateEngineerHTML(engineer) {
 
                     <ul class="list-group">
                         <li class="list-group-item">ID: ${engineer.id}</li>
-                        <li class="list-group-item">Email: <a href="${engineer.getEmail()}" target="blank">${engineer.email}</a></li>
-                        <li class="list-group-item">GitHub: <a href="${engineer.getGithub()}" target="blank">${engineer.githubUsername}</a></li>
+                        <li class="list-group-item">Email: <a href="${engineer.getEmail()}" target="blank">${
+    engineer.email
+  }</a></li>
+                        <li class="list-group-item">GitHub: <a href="${engineer.getGithub()}" target="blank">${
+    engineer.githubUsername
+  }</a></li>
                     </ul>
                 </div>
             </div>`;
 }
 
 function generateInternHTML(intern) {
-  return `  <div class="card col-3 p-0 m-1">
+  return `  
+  <div class="card col-3 p-0 m-1">
                 <div class="card-header bg-dark">
                     <h2 class="card-title">${intern.name}</h2>
                     <p>${intern.getRole()}</p>
@@ -43,8 +54,12 @@ function generateInternHTML(intern) {
 
                     <ul class="list-group">
                         <li class="list-group-item">ID: ${intern.id}</li>
-                        <li class="list-group-item">Email: <a href="${intern.getEmail()}" target="blank">${intern.email}</a></li>
-                        <li class="list-group-item">School: ${intern.school}</li>
+                        <li class="list-group-item">Email: <a href="${intern.getEmail()}" target="blank">${
+    intern.email
+  }</a></li>
+                        <li class="list-group-item">School: ${
+                          intern.school
+                        }</li>
                     </ul>
                 </div>
             </div>`;
